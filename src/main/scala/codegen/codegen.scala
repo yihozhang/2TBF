ac18a62a309c10f15c81b685a310a1b07a13dcf8
Yihong Zhang <yihong@yihongzhang.localdomain>
2019-12-28 20:22:43 -0500

    impl moveToGlobalZero and restoreToLocal

--- src/main/scala/codegen/codegen.scala
+++ src/main/scala/codegen/codegen.scala
@@ -1,13 +1,22 @@
 package ttbf.codegen
 import ttbf.common.AST._
+import ttbf.common.InstrM._
 
 object Codegen {
-  private type Env = Map[String, Var]
-  private case class LocalEnv(env: Env)
-  private case class GlobalEnv(env: Env)
-  private class Var(val name: String, val size: Int, val typ: ASTVarType) {
-    def this(varDecl: ASTVarDecl) {
-      this(varDecl.name, calcSize(varDecl.varType), varDecl.varType)
+  private class Env(env: Map[ASTId, Var]) {
+    def contains(id: ASTId) = env.contains(id)
+    def apply(id: ASTId)    = env(id)
+  }
+  private case class LocalEnv(env: Map[ASTId, Var])  extends Env(env)
+  private case class GlobalEnv(env: Map[ASTId, Var]) extends Env(env)
+  private class Var(
+      val name: ASTId,
+      val p: Int,
+      val size: Int,
+      val typ: ASTVarType
+  ) {
+    def this(varDecl: ASTVarDecl, p: Int) {
+      this(varDecl.varName, p, calcSize(varDecl.varType), varDecl.varType)
     }
   }
   private def calcSize(typ: ASTVarType): Int = typ match {
@@ -16,39 +25,79 @@ object Codegen {
     case ASTChar => 1
     case ASTInt  => 1
   }
-  private object Code {
-    val SET_ZERO = "!0"
-    val ELIM_ZERO = "!0"
-    val MOVE_TO_ZERO = "?0-1[o0<1?0-1]"
-  }
+
+  private def moveTo(pos: Pos): InstrM[Unit] = ???
+  private def moveToLocal(p: Int): InstrM[Unit] =
+    for {
+      a <- Op.getPos
+      _ <- if (a.isLocal) {
+        Op.moveRight(p - a.p)
+      } else {
+        for {
+          _ <- Op.restoreToLocal
+          b <- Op.getPos
+          _ <- Op.moveRight(p - b.p)
+        } yield ()
+      }
+    } yield ()
+  private def moveToGlobal(p: Int): InstrM[Unit] = 
+    for {
+      a <- Op.getPos
+      _ <- if (a.isLocal) {
+        for {
+          _ <- Op.moveToGlobalZero
+          _ <- Op.moveRight(p)
+        } yield ()
+      } else {
+        Op.moveRight(p - a.p)
+      }
+    } yield ()
 }
 
 class Codegen(prog: ASTProg) {
   import Codegen._
-  private lazy val globalVEnv = prog.globalVars.map { v =>
-    (v.name, new Codegen.Var(v))
-  } toMap
+  private lazy val globalVEnv = {
+    var cur = 0
+    val env = prog.globalVars
+      .map(astv => {
+        val v = new Codegen.Var(astv, cur)
+        cur += v.size
+        (v.name, v)
+      })
+      .toMap
+    GlobalEnv(env)
+  }
+
   private def genStmts(
       stmts: List[ASTStmt]
-  )(implicit local: LocalEnv, global: GlobalEnv): List[String] = {
-    var pos = 0
-    stmts map {
-      case ASTAsg(lval, rval) => {
-        val lvalCode = genExpr(lval)
-        val rvalCode = genExpr(rval)
-        ???
-      }
-      case ASTRead(expr)  => ???
-      case ASTWrite(expr) => ???
-    }
+  )(implicit local: LocalEnv, global: GlobalEnv) = {
+    val instrs = stmts map {
+          case ASTAsg(lval, rval) =>
+            for {
+              _ <- genExpr(lval)
+              _ <- genExpr(rval)
+              // ???
+            } yield ()
+          case ASTRead(expr)  => ???
+          case ASTWrite(expr) => ???
+        }
+    ???
   }
   private def genExpr(
       expr: ASTExpr
-  )(implicit local: LocalEnv, global: GlobalEnv): List[String] = expr match {
+  )(implicit local: LocalEnv, global: GlobalEnv): InstrM[Unit] = expr match {
     case ASTIdxedVar(id, idx) => ???
-    case ASTConst(v)          => List(f"u$v%d")
-    case ASTVar(id)           => ???
-    case ASTPlus(lv, rv)      => ???
-    case ASTMinus(lv, rv)     => ???
+    case ASTConst(v)          => Op.push(f"u$v%d")
+    case ASTVar(id) => {
+      if (local contains id) {
+        moveToLocal(local(id).p)
+      } else if (global contains id) {
+        moveToGlobal(global(id).p)
+      } else {
+        throw new IllegalArgumentException("Can't find variables")
+      }
+    }
+    case ASTPlus(lv, rv)  => ???
+    case ASTMinus(lv, rv) => ???
   }
 }
--- src/main/scala/common/InstrM.scala
+++ src/main/scala/common/InstrM.scala
@@ -1,45 +1,118 @@
 package ttbf.common
 
+import scala.collection.immutable.Nil
 
 package object InstrM {
-    case class Pos(isLocal: Boolean, p: Int) {
-        def isGlobal = !isLocal
-        def toLeft(x: Int) = Pos(isLocal, p - x)
-        def toRight(x: Int) = toLeft(-x)
+
+  object PositionState extends Enumeration {
+    type PositionState = Value
+    val GLOBAL, LOCAL = Value
+  }
+  case class Pos(s: PositionState.PositionState, p: Int) {
+    val isGlobal        = s == PositionState.GLOBAL
+    val isLocal         = s == PositionState.LOCAL
+    def toLeft(x: Int)  = Pos(s, p - x)
+    def toRight(x: Int) = toLeft(-x)
+  }
+
+  case class StateM[S, A](runState: S => (A, S)) {
+    def flatMap[B](f: A => StateM[S, B]) = StateM[S, B] { (s: S) =>
+      {
+        val (v, ss) = this runState s
+        f(v) runState ss
+      }
     }
-    case class StateM[S,A](runState: S => (A, S)) {
-        def flatMap[B](f: A => StateM[S,B]) = StateM[S,B] {
-            (s:S) => {
-                val (v, ss) = this runState s
-                f(v) runState ss
-            }
+    def map[B](f: A => B) = flatMap { (a: A) =>
+      StateM.unit(f(a))
+    }
+  }
+  object StateM {
+    def unit[S, A](a: A) = StateM[S, A] { (s: S) =>
+      (a, s)
+    }
+  }
+
+  case class InstrState(val pos: Pos, val instr: List[String], val posSaved: Option[Pos] = None) {
+    def toLeft(x: Int)    = InstrState(pos.toLeft(x), instr)
+    def toRight(x: Int)   = InstrState(pos.toRight(x), instr)
+    def push(str: String) = InstrState(pos, str :: instr)
+    def pop(str: String) = instr match {
+      case hd :: tl => (hd, InstrState(pos, tl))
+      case Nil      => throw new IllegalArgumentException("can't pop out an empty instr stack")
+    }
+    def top = instr match {
+      case hd :: tl => (hd, this)
+      case Nil      => throw new IllegalArgumentException("can't get the top of an empty instr stack")
+    }
+    def setPos(newPos: Pos) = InstrState(newPos, instr)
+    def savePos()           = InstrState(pos, instr, Some(pos))
+    def restoreSavedPos() = posSaved match {
+      case None        => throw new IllegalStateException("try to restore to a local state that does not exist")
+      case Some(value) => InstrState(value, instr)
+    }
+  }
+  type InstrM[A] = StateM[InstrState, A]
+
+  private object Code {
+    val SET_ZERO  = "!0"
+    val ELIM_ZERO = "!0"
+    // val MOVE_TO_ZERO = "?0-1[o0<1?0-1]"
+    // NOTE: only use this when move from local to global scope
+    val MOVE_FROM_LOCAL_TO_GLOBAL_ZERO    = "u0?0-1[o0<1+1?0-1]o"
+    val RESTORE_FROM_GLOBAL_ZERO_TO_LOCAL = "u[>1-1]o0"
+  }
+
+  object Op {
+    def moveLeft(x: Int): InstrM[Unit] =
+      if (x < 0)
+        moveRight(-x)
+      else
+        StateM { (s: InstrState) =>
+          ((), s.toLeft(x).push(f"<$x%d"))
         }
-        def map[B](f: A => B) = flatMap { (a: A) => StateM.pure(f(a)) }
-    }
-    object StateM {
-        def pure[S, A](a: A) = StateM[S,A] {
-            (s:S) => (a, s)
+
+    def moveRight(x: Int): InstrM[Unit] =
+      if (x < 0)
+        moveLeft(-x)
+      else
+        StateM { (s: InstrState) =>
+          ((), s.toRight(x).push(f">$x%d"))
         }
+    val moveToGlobalZero = StateM { (s: InstrState) =>
+      if (s.pos.isGlobal)
+        ((), s.toLeft(s.pos.p).push(f"<${s.pos.p}%d"))
+      else
+        (
+          (),
+          s.savePos()
+            .setPos(Pos(PositionState.GLOBAL, 0))
+            .push(Code.MOVE_FROM_LOCAL_TO_GLOBAL_ZERO)
+        )
     }
-    // case class StateT[S, A](val runStateT: S => Writer[(A, S)]) {
-    //     def flatMap[B](f: A => StateT[S,B]) = 
-    //         (s: S) => for (
-    //             t <- this.runStateT(s)
-    //          ) yield {
-    //             val (v, ss) = t
-    //             f(v).runStateT(ss)
-    //         }
-    // }
-    // object StateT {
-    //     def lift[S,A](c: Writer[A]): StateT[S,A] = StateT { 
-    //         (s: S) => c flatMap { (a: A) => Writer.pure (a,s) }
-    //     }
-    // }
-    
-    object Instr {
-        type InstrM[A] = StateM[Pos, A]
-        // def moveLeft(x: Int): InstrM[Unit] = StateT { (pos: Pos) => Writer.pure(((), pos.toLeft(x)))}
-        // def moveRight(x: Int): InstrM[Unit] = StateT { (pos: Pos) => Writer.pure(((), pos.toRight(x)))}
-        // def tell(x:String): InstrM[Unit] = StateT.lift(Writer.tell(x))
+    val restoreToLocal: InstrM[Unit] = StateM { (s: InstrState) =>
+      if (s.pos.isLocal) {
+        throw new IllegalArgumentException("apply restore at a local state")
+      } else {
+        (
+          (),
+          s.push(f"<${s.pos.p}%d")
+            .push(Code.RESTORE_FROM_GLOBAL_ZERO_TO_LOCAL)
+            .restoreSavedPos()
+        )
+      }
     }
+
+    // def setPos(pos: Pos) = StateM { (s: InstrState) =>
+    //   ((), s.setPos(pos))
+    // }
+    val getPos = StateM { (s: InstrState) =>
+      (s.pos, s)
+    }
+    def push(instr: String) = StateM { (s: InstrState) =>
+      ((), s.push(instr))
+    }
+    def pop(instr: String) = StateM { (s: InstrState) =>
+      s.pop(instr)
+    }
+  }
 }
